1858eaf552d5a96aa6d4e8b61fbbf242bd29684e
Sean <seankim912@yahoo.com>
2022-09-26 02:25:25 -0500

    Add User input validation on user signup backend endpoint

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -4,8 +4,49 @@ const express = require('express');
 const { setTokenCookie, requireAuth } = require('../../utils/auth');
 const { User } = require('../../db/models');
 const { validationResult } = require('express-validator');
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
 
 const router = express.Router();
+
+// middleware for formatting errors from express-validator middleware
+// (to customize, see express-validator's documentation)
+const handleValidationErrors = (req, _res, next) => {
+    const validationErrors = validationResult(req);
+
+    if (!validationErrors.isEmpty()) {
+        const errors = validationErrors
+            .array()
+            .map((error) => `${error.msg}`);
+
+        const err = Error('Bad request.');
+        err.errors = errors;
+        err.status = 400;
+        err.title = 'Bad request.';
+        next(err);
+    }
+    next();
+};
+
+const validateSignup = [
+    check('email')
+        .exists({ checkFalsy: true })
+        .isEmail()
+        .withMessage('Please provide a valid email.'),
+    check('username')
+        .exists({ checkFalsy: true })
+        .isLength({ min: 4 })
+        .withMessage('Please provide a username with at least 4 characters.'),
+    check('username')
+        .not()
+        .isEmail()
+        .withMessage('Username cannot be an email.'),
+    check('password')
+        .exists({ checkFalsy: true })
+        .isLength({ min: 6 })
+        .withMessage('Password must be 6 characters or more.'),
+    handleValidationErrors
+];
 
 router.post(
     '/',
@@ -34,24 +75,20 @@ router.get(
     }
 );
 
-// middleware for formatting errors from express-validator middleware
-// (to customize, see express-validator's documentation)
-const handleValidationErrors = (req, _res, next) => {
-    const validationErrors = validationResult(req);
+router.post(
+    '/',
+    validateSignup,
+    async (req, res) => {
+        const { email, password, username } = req.body;
+        const user = await User.signup({ email, username, password });
 
-    if (!validationErrors.isEmpty()) {
-        const errors = validationErrors
-            .array()
-            .map((error) => `${error.msg}`);
+        await setTokenCookie(res, user);
 
-        const err = Error('Bad request.');
-        err.errors = errors;
-        err.status = 400;
-        err.title = 'Bad request.';
-        next(err);
+        return res.json({
+            user,
+        });
     }
-    next();
-};
+);
 
 module.exports = {
     router
