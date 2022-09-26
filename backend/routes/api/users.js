3093b80cc5a78195ed560ff28230c9325f8ad712
Sean <seankim912@yahoo.com>
2022-09-26 01:52:30 -0500

    Validation middleware

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,6 +1,61 @@
 // backend/routes/api/users.js
-const express = require('express')
+const express = require('express');
+
+const { setTokenCookie, requireAuth } = require('../../utils/auth');
+const { User } = require('../../db/models');
+const { validationResult } = require('express-validator');
+
 const router = express.Router();
 
-module.exports = router;
+router.post(
+    '/',
+    async (req, res) => {
+        const { email, password, username } = req.body;
+        const user = await User.signup({ email, username, password });
 
+        await setTokenCookie(res, user);
+
+        return res.json({
+            user
+        });
+    }
+);
+
+router.get(
+    '/',
+    restoreUser,
+    (req, res) => {
+        const { user } = req;
+        if (user) {
+            return res.json({
+                user: user.toSafeObject()
+            });
+        } else return res.json({});
+    }
+);
+
+// middleware for formatting errors from express-validator middleware
+// (to customize, see express-validator's documentation)
+const handleValidationErrors = (req, _res, next) => {
+    const validationErrors = validationResult(req);
+
+    if (!validationErrors.isEmpty()) {
+        const errors = validationErrors
+            .array()
+            .map((error) => `${error.msg}`);
+
+        const err = Error('Bad request.');
+        err.errors = errors;
+        err.status = 400;
+        err.title = 'Bad request.';
+        next(err);
+    }
+    next();
+};
+
+module.exports = {
+    router
+},
+{
+    handleValidationErrors
+};
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -20,8 +20,8 @@ router.get("/api/csrf/restore", (req,res) => {
 });
 
 // GET /api/set-token-cookie
-const { setTokenCookie } = require('../../utils/auth.js');
-const { User } = require('../../db/models');
+const { setTokenCookie } = require('../utils/auth.js');
+const { User } = require('../db/models');
 router.get('/set-token-cookie', async (_req, res) => {
     const user = await User.findOne({
         where: {
@@ -33,7 +33,7 @@ router.get('/set-token-cookie', async (_req, res) => {
 });
 
 // GET /api/restore-user
-const { restoreUser } = require('../../utils/auth.js');
+const { restoreUser } = require('../utils/auth.js');
 
 router.use(restoreUser);
 
@@ -49,7 +49,7 @@ router.use(restoreUser);
 // ...
 
 // GET /api/require-auth
-const { requireAuth } = require('../../utils/auth.js');
+const { requireAuth } = require('../utils/auth.js');
 router.get(
   '/require-auth',
   requireAuth,
