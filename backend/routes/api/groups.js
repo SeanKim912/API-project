da303eabd4ebadf2a3ccd26cc19dc9be9c42d3f8
Sean <seankim912@yahoo.com>
2022-10-30 06:01:05 -0500

    groupId env variable

--- backend/routes/api/groups.js
+++ backend/routes/api/groups.js
@@ -7,8 +7,9 @@ const { requireAuth } = require('../../utils/auth');
 // Add an Image to a Group based on the Group's id
 router.post('/:groupId/images', async (req, res) => {
     const { url, preview } = req.body;
+    const { groupId } = req.params;
 
-    const group = await Group.findByPk(req.params.groupId);
+    const group = await Group.findByPk(groupId);
 
     if (!group) {
         res.statusCode = 404;
@@ -19,12 +20,16 @@ router.post('/:groupId/images', async (req, res) => {
     }
 
     const newGroupImage = await GroupImage.create({
-        groupId: group.id,
+        groupId,
         url,
         preview
     });
 
-    res.json(newGroupImage);
+    res.json({
+        id: newGroupImage.id,
+        url: newGroupImage.url,
+        preview: newGroupImage.preview
+    });
 });
 
 // Return all groups
