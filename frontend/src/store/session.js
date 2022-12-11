1c8973cfed09dfd001ac85d49d34e85bc0d357ab
Sean <seankim912@yahoo.com>
2022-12-10 20:29:46 -0600

    Phase 3

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -36,6 +36,15 @@ export const login = (user) => async (dispatch) => {
 };
 
 
+export const logout = () => async (dispatch) => {
+    const response = await csrfFetch('/api/session', {
+        method: 'DELETE',
+    });
+    dispatch(removeUser());
+    return response;
+};
+
+
 export const signup = (user) => async (dispatch) => {
     const { username, firstName, lastName, email, password } = user;
     const response = await csrfFetch("/api/users", {
